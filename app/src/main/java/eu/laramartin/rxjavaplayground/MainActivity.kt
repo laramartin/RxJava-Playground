ddd32cc7ed12f09117f3f591a5bb6e5ddb1be9fe
Lara Martin <larikimc@gmail.com>
2018-02-23 09:51:15 +0100

    Simple examples

--- app/src/main/java/eu/laramartin/rxjavaplayground/MainActivity.kt
+++ app/src/main/java/eu/laramartin/rxjavaplayground/MainActivity.kt
@@ -1,12 +1,24 @@
 package eu.laramartin.rxjavaplayground
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import io.reactivex.Observable
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Check the RxJavaTest for simple examples
+
+        Observable.just(42)
+                .subscribe { log(it) }
+
+    }
+
+    private fun log(it: Any) {
+        Log.d("RxJava", "Log: $it")
     }
 }
---
+++ app/src/test/java/eu/laramartin/rxjavaplayground/RxJavaTest.kt
@@ -0,0 +1,25 @@
+package eu.laramartin.rxjavaplayground
+
+import io.reactivex.Observable
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+// Run me!
+class RxJavaTest {
+
+    @Test
+    fun `simple observable print`() {
+        // Look at the test output
+        Observable.just(42).subscribe { print(it) }
+    }
+
+    @Test
+    fun `simple observable with test`() {
+        // How to test in RxJava2
+        val test = Observable.just(42).test()
+
+        // Use "values" to get all the observed values
+        assertEquals(42, test.values()[0])
+    }
+
+}
